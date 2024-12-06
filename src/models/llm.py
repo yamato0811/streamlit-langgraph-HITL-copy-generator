027df211ba5075cda98ac55fe6c974605082b0df
renya <rsmilek5492@gmail.com>
2024-12-06 11:00:51 +0900

    Add support for Claude-3-5 model in LLM initialization

--- src/models/llm.py
+++ src/models/llm.py
@@ -1,3 +1,5 @@
+from langchain_aws import ChatBedrock
+from langchain_core.language_models import BaseChatModel
 from langchain_core.language_models.base import LanguageModelInput
 from langchain_core.messages import BaseMessage
 from langchain_openai import ChatOpenAI
@@ -8,9 +10,15 @@ class LLM:
     def __init__(self, model_name: str, temperature: float):
         self.model = self._initialize_llm(model_name, temperature)
 
-    def _initialize_llm(self, model_name: str, temperature: float) -> ChatOpenAI:
+    def _initialize_llm(self, model_name: str, temperature: float) -> BaseChatModel:
         if model_name == "gpt-4o":
             return ChatOpenAI(model="gpt-4o", temperature=temperature)
+        elif model_name == "claude-3-5":
+            return ChatBedrock(
+                model="anthropic.claude-3-5-sonnet-20241022-v2:0",
+                temperature=temperature,
+                region="us-west-2",
+            )
         else:
             raise ValueError(f"Model name {model_name} not supported.")
 
